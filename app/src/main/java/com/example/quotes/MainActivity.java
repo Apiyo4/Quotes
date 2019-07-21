7a4cc7496875a07261170ec74ea01734a9545b8e
Annette Odingo <odingoannette@gmail.com>
2019-07-21 10:37:02 +0300

    Add three activities so that when buttons are clicked goes to the activities

--- app/src/main/java/com/example/quotes/MainActivity.java
+++ app/src/main/java/com/example/quotes/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.quotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private  Button mAddButton;
+    private  Button mReadButton;
+    private  Button mPicButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAddButton = (Button) findViewById(R.id.addButton);
+        mAddButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addIntent = new Intent(MainActivity.this, AddActivity);
+                startActivity(addIntent);
+
+            }
+        });
+        mReadButton = (Button) findViewById(R.id.readButton);
+        mReadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addIntent = new Intent(MainActivity.this, ReadActivity);
+                startActivity(addIntent);
+
+
+            }
+        });
+        mPicButton = (Button) findViewById(R.id.picButton);
+        mPicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent addIntent = new Intent(MainActivity.this, PicActivity);
+                startActivity(addIntent);
+
+
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/quotes/ReadActivity.java
@@ -0,0 +1,14 @@
+package com.example.quotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read);
+    }
+}
