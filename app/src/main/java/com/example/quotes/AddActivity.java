a054e784f49d81a61709582874924a91cb06d230
Annette Odingo <odingoannette@gmail.com>
2019-07-21 11:49:09 +0300

    Confirm AddActivity sends data to ReadActivity

--- app/src/main/java/com/example/quotes/AddActivity.java
+++ app/src/main/java/com/example/quotes/AddActivity.java
@@ -11,19 +11,23 @@ import android.widget.EditText;
 public class AddActivity extends AppCompatActivity {
     private Button mAddQuoteButton;
     private EditText mAuthor;
+    private EditText mQuote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         mAuthor= (EditText) findViewById(R.id.author);
+        mQuote= (EditText) findViewById(R.id.quote);
         mAddQuoteButton = (Button) findViewById(R.id.addQuoteButton);
         mAddQuoteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String author = mAuthor.getText().toString();
+                String quote = mQuote.getText().toString();
                 Intent addQuoteIntent = new Intent(AddActivity.this, ReadActivity.class);
                 addQuoteIntent.putExtra("author", author);
+                addQuoteIntent.putExtra("quote", quote);
                 startActivity(addQuoteIntent);
             }
         });
--- app/src/main/java/com/example/quotes/ReadActivity.java
+++ app/src/main/java/com/example/quotes/ReadActivity.java
@@ -2,13 +2,23 @@ package com.example.quotes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReadActivity extends AppCompatActivity {
-
+    private TextView mNewAuthor;
+    private TextView mNewQuote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read);
+        mNewAuthor = (TextView) findViewById(R.id.newAuthor);
+        mNewQuote= (TextView) findViewById(R.id.newQuote);
+        Intent addQuoteIntent = getIntent();
+        String author = addQuoteIntent.getStringExtra("author");
+        String quote = addQuoteIntent.getStringExtra("quote");
+        mNewAuthor.setText(author);
+        mNewQuote.setText(quote);
     }
 }
