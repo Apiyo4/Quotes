f271d623cc7ec8b3bfdaa78b32d28aa714a87527
Annette Odingo <odingoannette@gmail.com>
2019-07-28 13:26:23 +0300

    Add data to Constants.java

--- app/src/main/java/com/example/quotes/Constants.java
+++ app/src/main/java/com/example/quotes/Constants.java
@@ -1,4 +1,7 @@
 package com.example.quotes;
 
 public class Constants {
+    public static final String QUOTE_BASE_URL = "http://quotes.stormconsultancy.co.uk/quotes.json";
+    public static final String QUOTE_ID_QUERY_PARAMETER = "id";
+
 }
