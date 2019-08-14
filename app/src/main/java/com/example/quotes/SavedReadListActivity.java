f6c186b1dc8cd4508560ab798870d1ce475ff0b5
Annette Odingo <odingoannette@gmail.com>
2019-08-14 16:51:34 +0300

    Create an activity to hold the quotes user will save

--- app/src/main/java/com/example/quotes/SavedReadListActivity.java
+++ app/src/main/java/com/example/quotes/SavedReadListActivity.java
@@ -1,14 +1,78 @@
 package com.example.quotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.quotes.models.Quote;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SavedReadListActivity extends AppCompatActivity {
+
+    private DatabaseReference mReadReference;
+    private FirebaseRecyclerAdapter<Quote, FirebaseReadViewHolder> mFirebaseAdapter;
+
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_saved_read_list);
+        setContentView(R.layout.activity_read);
+        ButterKnife.bind(this);
+
+        mReadReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_QUOTES);
+        setUpFirebaseAdapter();
+    }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Quote> options =
+                new FirebaseRecyclerOptions.Builder<Quote>()
+                        .setQuery(mReadReference, Quote.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Quote, FirebaseReadViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseReadViewHolder firebaseReadViewHolder, int position, @NonNull Quote quote) {
+                firebaseReadViewHolder.bindQuote(quote);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseReadViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.read_list_item, parent, false);
+                return new FirebaseReadViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
     }
 }
