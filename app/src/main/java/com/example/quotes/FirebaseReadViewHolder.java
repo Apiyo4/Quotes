74be44e02d0201379bfacbcc89472ef5106a3560
Annette Odingo <odingoannette@gmail.com>
2019-08-15 12:24:21 +0300

    Create SimpleItemTouchHelperCallback.java

--- app/src/main/java/com/example/quotes/FirebaseReadViewHolder.java
+++ app/src/main/java/com/example/quotes/FirebaseReadViewHolder.java
@@ -2,6 +2,7 @@ package com.example.quotes;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,6 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.quotes.models.Quote;
+import com.example.quotes.util.ItemTouchHelperViewHolder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -22,7 +24,7 @@ import org.parceler.Parcels;
 
 import java.util.ArrayList;
 
-public class FirebaseReadViewHolder extends RecyclerView.ViewHolder {
+public class FirebaseReadViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
 
     private static final int MAX_WIDTH = 200;
     private static final int MAX_HEIGHT = 200;
@@ -35,18 +37,25 @@ public class FirebaseReadViewHolder extends RecyclerView.ViewHolder {
         super(itemView);
         mView = itemView;
         mContext = itemView.getContext();
-
     }
 
     public void bindQuote(Quote quote) {
 
         mAuthor= (TextView) mView.findViewById(R.id.authorTextView);
         mQuoteTextView= (TextView) mView.findViewById(R.id.quoteTextView);
-
-
-
         mAuthor.setText(quote.getAuthor());
         mQuoteTextView.setText(quote.getQuote());
 
+    }
+    @Override
+    public void onItemSelected() {
+        Log.d("Animation", "onItemSelected");
+        // we will add animations here
+    }
+
+    @Override
+    public void onItemClear() {
+        Log.d("Animation", "onItemClear");
+        // we will add animations here
     }
 }
